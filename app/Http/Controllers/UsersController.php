f26d5ed2238fe0ddfba2e8c9a774dc383d24f64a
KD <861529741@qq.com>
2017-11-23 08:16:15 +0000

    Fix: update form validate rules

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,7 +20,7 @@ class UsersController extends Controller
       $this->validate($request, [
         'name' => 'required|max:50',
         'email' => 'required|email|unique:users|max:255',
-        'password' => 'required'
+        'password' => 'required|confirmed'
       ]);
 
       $user = User::create([
